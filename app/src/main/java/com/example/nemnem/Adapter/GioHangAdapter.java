9e3c2105f4f4da3e993f9e3cfc162d045082f5ba
ducvtph25202 <ducvtph25202@fpt.edu.vn>
2022-12-02 13:18:24 +0700

    02/12/2022

--- app/src/main/java/com/example/nemnem/Adapter/GioHangAdapter.java
+++ app/src/main/java/com/example/nemnem/Adapter/GioHangAdapter.java
@@ -1,11 +1,14 @@
 package com.example.nemnem.Adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -39,7 +42,40 @@ public class GioHangAdapter extends RecyclerView.Adapter<GioHangAdapter.ViewHold
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        int index = position;
+        GioHang objGHang = list.get(position);
+        holder.txtsp.setText(list.get(position).getTensp());
+        holder.slsp.setText(String.valueOf(list.get(position).getSoluong()));
+        holder.dgsp.setText(String.valueOf(list.get(position).getDongia()));
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setTitle("Xóa");
+                builder.setMessage("Xác nhận xóa?");
+                builder.setPositiveButton("Xóa", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        int kq = gioHangDAO.delete(objGHang);
+                        if (kq>0){
+                            list.remove(index);
+                            notifyDataSetChanged();
+                            Toast.makeText(context, "Xóa thành công", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(context, "Xóa thất bại", Toast.LENGTH_SHORT).show();
+                        } dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("Hủy", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create();
+                builder.show();
+            }
+        });
     }
 
     @Override
